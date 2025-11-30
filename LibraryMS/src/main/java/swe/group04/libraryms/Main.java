3891e7224eb31756afe5104027f6afbd2fcaefbe
mattiajb <bavaromg@gmail.com>
2025-11-30 15:50:02 +0100

    Initial project structure added (LibraryMS)

---
+++ LibraryMS/src/main/java/swe/group04/libraryms/Main.java
@@ -0,0 +1,25 @@
+package swe.group04.libraryms;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(
+                getClass().getResource("/swe/group04/libraryms/view/main.fxml")
+        );
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Library Management System");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
