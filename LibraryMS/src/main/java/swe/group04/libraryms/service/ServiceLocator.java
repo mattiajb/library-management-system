278b3a6c2048f9b95992e4c215dec9d472ec6a93
AndreaBotta333 <anbotta2005@gmail.com>
2025-12-12 21:17:25 +0100

    Add Doxygen comment for ServiceLocator

--- LibraryMS/src/main/java/swe/group04/libraryms/service/ServiceLocator.java
+++ LibraryMS/src/main/java/swe/group04/libraryms/service/ServiceLocator.java
@@ -1,3 +1,18 @@
+/**
+ * @file ServiceLocator.java
+ * @brief Service Locator per l'accesso centralizzato ai servizi applicativi.
+ *
+ * Fornisce punti di accesso globali ai servizi di applicazione
+ *
+ * Scelte architetturali:
+ * - I servizi vengono istanziati una sola volta (static final) e condivisi.
+ * - Tutti i service specifici condividono la stessa istanza di LibraryArchiveService,
+ *   quindi operano sul medesimo oggetto LibraryArchive.
+ *
+ * @note Il caricamento da file dell'archivio non avviene qui: viene
+ *       invocato all'avvio dell'applicazione (es. nel controller principale)
+ *       tramite LibraryArchiveService::loadArchive().
+ */
 package swe.group04.libraryms.service;
 
 import swe.group04.libraryms.models.LibraryArchive;
@@ -32,4 +47,3 @@ public class ServiceLocator {
         return loanService;
     }
 }
-
